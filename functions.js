4f75128c4b660bbb960c662574c43f1dd6474224
Alex Winstanley <ajgwinstanley@hotmail.com>
2023-11-15 11:55:10 +0000

    6th

--- functions.js
+++
@@ -1,9 +0,0 @@
-import { runChallenge } from './inquirer.js'
-import { Door,Lock } from "./classes.js"
-
-const completeAChallenge=()=>{
-    Door()
-}
-completeAChallenge()
-
-export {runChallenge}
